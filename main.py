6da306f3ba96cf7389104f0f766d89c5f66dcfc1
oekrice <oliverrrice1998@gmail.com>
2024-11-13 11:18:42 +0000

    Decent ringing up

--- main.py
+++ main.py
@@ -45,6 +45,9 @@ else:
         bell.bell_angle = uniform(-np.pi-0.95*bell.stay_angle, -np.pi-bell.stay_angle)
         bell.clapper_angle = bell.bell_angle - bell.clapper_limit + 0.01
 
+bell.bell_angle = 0.0
+bell.clapper_angle = 0.0
+
 dp = display_tools(phy, bell)
 
 bell.sound = pygame.mixer.Sound("bellsound_deep.wav")
